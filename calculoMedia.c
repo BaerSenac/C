ef4664680738756ee7bebd64945156db4402026a
JBAER <368271Qualidade!>
2022-04-25 08:10:37 -0300

    soma e pow

---
+++ calculoMedia.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+float A, B;
+float media;
+
+printf("\n Digite um valor:");
+scanf("%f", &A);
+printf("Digite um segundo valor: ");
+scanf("%f", &B);
+media = (A + B) / 2;
+printf("\n resultado de A + B: %f", media);
+return 0;
+
+}
